b90787b79e5465b91238cccca617ea6bb79e0595
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2026-03-13 20:53:47 +0200

    Added main code for lab3

---
+++ Lab3/TrapeziumProject/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TrapeziumProject
+{
+    public partial class Form1 : Form
+    {
+        public Graphics graph;
+        public Pen pen1, pen2;
+        public int scale = 5;
+        private List<Trapezium> trapeziums = new List<Trapezium>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.White;
+            this.Size = new Size(900, 700);
+
+            pen1 = new Pen(Color.Black, 2);
+            pen2 = new Pen(Color.White, 2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graph == null && pictureBox1 != null)
+                graph = pictureBox1.CreateGraphics();
+
+            int trapCount = Trapezium.TrapeziumCount;
+
+            int centerX = 80 + (trapCount * 100);
+            int centerY = 250;
+            Point center = new Point(centerX, centerY);
+
+            int sideA = 50 + trapCount * 8;
+            int sideB = 100 + trapCount * 8;
+            int height = 80 + trapCount * 5;
+
+            Trapezium newTrap = new Trapezium(center, sideA, sideB, height, this);
+            trapeziums.Add(newTrap);
+
+            label1.Text = $"Трапецій створено: {Trapezium.TrapeziumCount}";
+
+            RedrawAll();
+        }
+
+        private void RedrawAll()
+        {
+            if (pictureBox1 == null)
+                return;
+
+            pictureBox1.Image = null;
+
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            g.Clear(Color.White);
+
+            foreach (Trapezium trap in trapeziums)
+            {
+                trap.Draw(g, pen1);
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+    }
+}
---
+++ Lab3/TrapeziumProject/Trapezium.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TrapeziumProject
+{
+    public class Trapezium
+    {
+        public static int TrapeziumCount = 0;
+
+        private Point center;
+        //Два розміри основ
+        private int sideA;
+        private int sideB;
+        private int height;
+        private Form1 form;
+
+        public Trapezium()//Конструктор БЕЗ параметрів
+        {
+            sideA = 0;
+            sideB = 0;
+            height = 0;
+        }
+
+        public Trapezium(Point c, int a, int b, int h, Form1 form1)// Конструктор З параметрами
+        {
+            ++TrapeziumCount;
+
+            center = c;
+            sideA = a;
+            sideB = b;
+            height = h;
+            form = form1;
+        }
+
+        // ===== МЕТОД МАЛЮВАННЯ =====
+        public void Draw(Graphics graph, Pen pen)
+        {
+            if (graph == null || pen == null)
+                return;
+
+            if (sideA <= 0 || sideB <= 0 || height <= 0)
+                return;
+
+            try
+            {
+                // Координати вершин трапеції
+                Point[] points = new Point[4]
+                {
+                    // Нижня основа (довша)
+                    new Point(center.X - sideB / 2, center.Y + height / 2),
+                    new Point(center.X + sideB / 2, center.Y + height / 2),
+                    
+                    // Верхня основа (коротша)
+                    new Point(center.X + sideA / 2, center.Y - height / 2),
+                    new Point(center.X - sideA / 2, center.Y - height / 2)
+                };
+
+                // Малюємо трапецію
+                graph.DrawPolygon(pen, points);
+
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка: {ex.Message}");
+            }
+        }
+
+        public void Show(Graphics graph, Pen pen)
+        {
+            Draw(graph, pen);
+        }
+
+        public void Hide(Graphics graph, Pen pen)
+        {
+            Pen hidePen = new Pen(Color.White, pen.Width);
+            Draw(graph, hidePen);
+        }
+
+        public int SideA { get { return sideA; } }
+        public int SideB { get { return sideB; } }
+        public int Height { get { return height; } }
+    }
+}
